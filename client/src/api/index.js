4650ae399f3386eca4762a4a74cc7bf39f177d55
Max <kakicpukic9@gmail.com>
2026-09-09 20:26:24 +0300

    fix: change API base URL to production Render

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({
-  baseURL: 'http://localhost:3002',
-})
+const API = axios.create({ baseURL: 'https://vagabond-anime-tracker.onrender.com' });
 
 // Автоматически добавляем токен к каждому запросу
 api.interceptors.request.use(config => {
